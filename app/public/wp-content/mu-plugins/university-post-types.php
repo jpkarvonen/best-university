0c01a3a34bd0fc76c8a703e0b89be73c4baf1318
John Karvonen <jpkarvonen@gmail.com>
2019-07-30 18:24:52 -0400

    add past events page also programs

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,4 +1,7 @@
-<?php function university_post_types() {
+<?php
+
+function university_post_types() {
+  //Event Post Type
   register_post_type('event', array(
     'supports' => array('title', 'editor', 'excerpt'),
     'has_archive' => true,
@@ -13,7 +16,24 @@
     ),
     'menu_icon' => 'dashicons-calendar'
   ));
+
+  //Program Post Type
+  register_post_type('program', array(
+    'supports' => array('title', 'editor'),
+    'has_archive' => true,
+    'rewrite' => array('slug' => 'programs'),
+    'public' => true,
+    'labels' => array (
+      'name' => 'Programs',
+      'add_new_item' => "Add New Program",
+      'edit_item' => "Edit Program",
+      'all_items' => "All Programs",
+      'singular_name' => "Program"
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
 }
+
 
 add_action('init', 'university_post_types');
 
