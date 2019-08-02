75e0d18ccdffc8fafabad4dbbc729fc499273585
John Karvonen <jpkarvonen@gmail.com>
2019-08-02 16:15:06 -0400

    complete user content

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -53,22 +53,41 @@ function university_post_types() {
     'menu_icon' => 'dashicons-awards'
   ));
 
-//Professor Post Type
-register_post_type('professor', array(
-  'show_in_rest' => true,
-  'supports' => array('title', 'editor', 'thumbnail'),
-  'public' => true,
-  'labels' => array (
-    'name' => 'Professor',
-    'add_new_item' => "Add New Professor",
-    'edit_item' => "Edit Professor",
-    'all_items' => "All Professors",
-    'singular_name' => "Professor"
-  ),
-  'menu_icon' => 'dashicons-welcome-learn-more'
-));
-}
+  //Professor Post Type
+  register_post_type('professor', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array (
+      'name' => 'Professor',
+      'add_new_item' => "Add New Professor",
+      'edit_item' => "Edit Professor",
+      'all_items' => "All Professors",
+      'singular_name' => "Professor"
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));
 
+
+  //Note Post Type
+  register_post_type('note', array(
+    'capability_type' => 'note',
+    'map_meta_cap' => true,
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'author'),
+    'public' => false,
+    'show_ui' => true,
+    'labels' => array (
+      'name' => 'My Notes',
+      'add_new_item' => "Add New Note",
+      'edit_item' => "Edit Note",
+      'all_items' => "All Note",
+      'singular_name' => "Note"
+      ),
+    'menu_icon' => 'dashicons-welcome-write-blog'
+  ));
+
+}
 
 add_action('init', 'university_post_types');
 
